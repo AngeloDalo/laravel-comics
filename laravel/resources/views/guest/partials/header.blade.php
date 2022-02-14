79a2818b6528fe3fe50c2a8e35f7e885ec34172a
AngeloDalo <angelo251158@gmail.com>
2022-02-14 15:51:18 +0100

    classe active

--- laravel/resources/views/guest/partials/header.blade.php
+++ laravel/resources/views/guest/partials/header.blade.php
@@ -8,16 +8,16 @@
             <img src="{{asset('img\dc-logo.png')}}" alt="dc-logo">
         </div>
         <ul>
-            <li><a href="{{route('characters')}}">CHARACTERS</a></li>
-            <li><a href="{{route('comics')}}">COMICS</a></li>
-            <li><a href="{{route('movies')}}">MOVIES</a></li>
-            <li><a href="{{route('tv')}}">TV</a></li>
-            <li><a href="{{route('games')}}">GAMES</a></li>
-            <li><a href="{{route('collectibles')}}">COLLECTIBLES</a></li>
-            <li><a href="{{route('videos')}}">VIDEOS</a></li>
-            <li><a href="{{route('fans')}}">FANS</a></li>
-            <li><a href="{{route('news')}}">NEWS</a></li>
-            <li><a href="{{route('shop')}}">SHOP</a></li>
+            <li class="{{ 'characters' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('characters')}}">CHARACTERS</a></li>
+            <li class="{{ 'comics' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('comics')}}">COMICS</a></li>
+            <li class="{{ 'movies' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('movies')}}">MOVIES</a></li>
+            <li class="{{ 'tv' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('tv')}}">TV</a></li>
+            <li class="{{ 'games' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('games')}}">GAMES</a></li>
+            <li class="{{ 'collectibles' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('collectibles')}}">COLLECTIBLES</a></li>
+            <li class="{{ 'videos' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('videos')}}">VIDEOS</a></li>
+            <li class="{{ 'fans' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('fans')}}">FANS</a></li>
+            <li class="{{ 'news' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('news')}}">NEWS</a></li>
+            <li class="{{ 'shop' === Route::currentRouteName() ? 'active' : '' }}"><a href="{{route('shop')}}">SHOP</a></li>
         </ul>
         <div class="search">
             Search
